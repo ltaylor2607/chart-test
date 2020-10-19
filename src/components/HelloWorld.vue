c8fd6c68e363743dd422740aa3091fe8693a8194
lynda_taylor <lynda.taylor@gdpr.school>
2020-10-19 08:28:48 +0100

    added charts to HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <JSCharting :options="pie"></JSCharting>
+    <JSCharting :options="bar"></JSCharting>
   </div>
 </template>
 
@@ -35,6 +11,88 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      pie: {
+        debug: true,
+        grid: {
+          enabled: true,
+        },
+        legend: {
+          position: 'inside left bottom',
+          template: '%value {%percentOfTotal:n1}% %icon %name'
+        },
+        title_position: 'center',
+        defaultSeries_type: 'pieDonut',
+        defaultPoint_label_text: '<b>%name</b>',
+        title_label_text: 'Countries GDP',
+        yAxis: { label_text: 'GDP', formatString: 'n'  },
+        series: [
+          {
+            name: 'Countries',
+            points: [
+              { name: 'United States', y: 5452500  },
+              { name: 'Canada', y: 786052  },
+              { name: 'United Kingdom', y: 477338  },
+              { name: 'Mexico', y: 155313  }
+            ]
+          }
+        ]
+      },
+      bar: {
+        debug: true, 
+        grid: {
+          enabled: true,
+        },
+        type: 'column', 
+        yAxis: { 
+          scale_type: 'stacked', 
+          label_text: 'Units Sold'
+        }, 
+        title_label_text: 'Acme Tool Sales', 
+        xAxis_label_text: 'Quarter', 
+        series: [ 
+          { 
+            name: 'Saw', 
+            id: 's1', 
+            points: [ 
+              { x: 'Q1', y: 230 }, 
+              { x: 'Q2', y: 240 }, 
+              { x: 'Q3', y: 267 }, 
+              { x: 'Q4', y: 238 } 
+            ] 
+          }, 
+          { 
+            name: 'Hammer', 
+            points: [ 
+              { x: 'Q1', y: 325 }, 
+              { x: 'Q2', y: 367 }, 
+              { x: 'Q3', y: 382 }, 
+              { x: 'Q4', y: 371 } 
+            ] 
+          }, 
+          { 
+            name: 'Grinder', 
+            points: [ 
+              { x: 'Q1', y: 285 }, 
+              { x: 'Q2', y: 292 }, 
+              { x: 'Q3', y: 267 }, 
+              { x: 'Q4', y: 218 } 
+            ] 
+          }, 
+          { 
+            name: 'Drill', 
+            points: [ 
+              { x: 'Q1', y: 185 }, 
+              { x: 'Q2', y: 192 }, 
+              { x: 'Q3', y: 198 }, 
+              { x: 'Q4', y: 248 } 
+            ] 
+          } 
+        ] 
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import JSCharting from "jscharting-vue"
+
+Vue.component("JSCharting", JSCharting)
 
 Vue.config.productionTip = false
 
